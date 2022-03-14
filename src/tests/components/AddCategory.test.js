26f2b5526eb97ae623cbcbd5a16be359ec434802
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-03-14 17:58:01 -0500

    Tests in AddCategory

---
+++ src/tests/components/AddCategory.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import {AddCategory} from "../../components/AddCategory";
+
+
+describe('Pruebas en <AddCategory />', () => {
+
+    const setCategories = () => {};
+
+    test('Debe mostrarse correctamente', () => {
+
+        const wrapper = shallow( <AddCategory setCategories={ setCategories } /> );
+        expect( wrapper ).toMatchSnapshot();
+
+    })
+
+})
