febed667b49d706da6361701941d355d541b19ac
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-03-15 16:06:21 -0500

    Test on GifGrid and add propTypes

---
+++ src/tests/components/GifGrid.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import {GifGrid} from '../../components/GifGrid';
+import {useFetchGifs} from '../../hooks/useFetchGifs';
+jest.mock('../../hooks/useFetchGifs');
+
+
+describe('Pruebas en <GifGrid />', () => {
+
+    const category = 'One Punch';
+
+    test('Debe mostrarse correctamente', () => {
+
+        useFetchGifs.mockReturnValue({
+            data: [],
+            loading: true
+        });
+
+        const wrapper = shallow( <GifGrid category={ category } /> );
+        expect( wrapper ).toMatchSnapshot();
+
+    });
+
+    test('Debe de mostrar items cuando se cargan imagenes useFetchGifs', () => {
+
+        const gifs = [{
+            id: 'ABC',
+            url: 'https://localhost/any.jpg',
+            title: 'anything',
+        }]
+
+        useFetchGifs.mockReturnValue({
+            data: gifs,
+            loading: false
+        });
+
+        const wrapper = shallow( <GifGrid category={ category } /> );
+
+        expect( wrapper ).toMatchSnapshot();
+
+    })
+
+})
