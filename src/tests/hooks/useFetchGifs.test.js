3d37f419693ad9146b41d5152d61c854697935d1
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-03-15 22:02:25 -0500

    Test on customHooks, waitForNextUpdate

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -4,15 +4,28 @@ import {renderHook} from "@testing-library/react-hooks";
 
 describe('Pruebas en el hook useFetchGifs', () => {
 
-    test('Debe retornar estado inicial', () => {
+    test('Debe retornar estado inicial', async() => {
 
-        const { result } = renderHook( () => useFetchGifs(' One Punch' ) );
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs(' One Punch' ) );
         const { data, loading } = result.current;
+        await waitForNextUpdate();
         //const { data, loading } = useFetchGifs( category );
         //console.log( data, loading );
         expect( data ).toEqual([]);
         expect( loading ).toBe(true);
 
-    })
+    });
+
+    test('Debe retornar un array de imgs y el loading en false', async() => {
+
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs(' One Punch' ) );
+        await waitForNextUpdate();
+
+        const { data, loading } = result.current;
+
+        expect( data.length ).toBe( 10 );
+        expect( loading ).toBe( false );
+
+    });
 
 })
